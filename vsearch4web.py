138b9c3fe17b04ca8bb961503d044be489a329d8
notaigbe <notaigbe@gmail.com>
2021-10-08 12:56:15 +0100

    First WebbApp Commit 8/10/2021

---
+++ vsearch4web.py
@@ -0,0 +1,33 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, request
+from vsearch import search4letters
+
+app = Flask(__name__)
+
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    # Use a breakpoint in the code line below to debug your script.
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results:'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+                           the_title=title,
+                           the_phrase=phrase,
+                           the_letters=letters,
+                           the_results=results,)
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html' :
+    return render_template('entry.html',
+                           the_title='Welcome to... web!')
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
